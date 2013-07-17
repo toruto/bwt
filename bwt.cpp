1043e64a50f451377163ac4f5dc6be8c3a20724d
toru igami <toru.igami@gmail.com>
2013-07-18 02:41:08 +0900

    impl lfmapping, initialy

--- bwt.cpp
+++ bwt.cpp
@@ -6,6 +6,10 @@ int main(int argc, char *argv[])
 {
      const std::string target = argv[1];
      bwt b;
-     std::cout << b.transform(target) << std::endl;
+	 const std::string bw_transformed =  b.transform(target); 
+     std::cout << "==================================" << std::endl; 
+     std::cout << bw_transformed << std::endl;
+     std::cout << "==================================" << std::endl; 
+     std::cout << b.inverse(bw_transformed) << std::endl;
      return 0;
 }
